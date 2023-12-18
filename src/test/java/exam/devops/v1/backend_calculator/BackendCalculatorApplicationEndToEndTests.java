ef145613117179d03297e4d30b7dd1280f2b9aa6
Ingabire Divine <divineingabire69@gmail.com>
2023-12-18 11:07:03 +0200

    updates():Added end to end test

--- src/test/java/exam/devops/v1/backend_calculator/BackendCalculatorApplicationEndToEndTests.java
+++ src/test/java/exam/devops/v1/backend_calculator/BackendCalculatorApplicationEndToEndTests.java
@@ -1,13 +1,26 @@
 package exam.devops.v1.backend_calculator;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
-class BackendCalculatorApplicationEndToEndTests {
+import static org.junit.Assert.assertEquals;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BackendCalculatorApplicationEndToEndTests {
+
+    @LocalServerPort
+    private String port = "8080";
+    private final TestRestTemplate restTemplate = new TestRestTemplate();
     @Test
-    void contextLoads() {
-    }
+    public void testApiEndpoint() {
+        String url = "http://localhost:" + port + "/api/v1/doMath_calculations";
 
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+
+        assertEquals(200, responseEntity.getStatusCodeValue());
+
+    }
 }
