b17c2d79f951e6e7ce454ab1934a7361de7048da
Ingabire Divine <divineingabire69@gmail.com>
2023-12-18 10:30:38 +0200

    added the intergration test

--- src/test/java/exam/devops/v1/backend_calculator/BackendCalculatorApplicationEndToEndTests.java
+++ src/test/java/exam/devops/v1/backend_calculator/BackendCalculatorApplicationEndToEndTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendCalculatorApplicationTests {
+class BackendCalculatorApplicationEndToEndTests {
 
     @Test
     void contextLoads() {
--- src/test/java/exam/devops/v1/backend_calculator/controllers/MathControllerIntegrationTest.java
+++ src/test/java/exam/devops/v1/backend_calculator/controllers/MathControllerIntegrationTest.java
@@ -1,4 +1,28 @@
 package exam.devops.v1.backend_calculator.controllers;
 
+import exam.devops.v1.backend_calculator.dtos.DoMathRequestDto;
+import exam.devops.v1.backend_calculator.payload.ApiResponse;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MathControllerIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void doMathOperation_Success() {
+        DoMathRequestDto dto = new DoMathRequestDto(34, 21, "+");
+        ResponseEntity<ApiResponse> response = this.restTemplate.postForEntity("/api/v1/doMath_calculations", dto, ApiResponse.class);
+        assertEquals(200, response.getStatusCode().value());
+    }
 }
